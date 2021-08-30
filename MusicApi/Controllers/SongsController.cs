48af97d68e1192a2662f656d151a4e802ba5beba
Kevin <kevin.shaughnessy18@gmail.com>
2021-08-29 22:52:26 -0400

    Created Put and Delete requests

--- MusicApi/Controllers/SongsController.cs
+++ MusicApi/Controllers/SongsController.cs
@@ -14,7 +14,7 @@ namespace MusicApi.Controllers
     {
         private static List<Song> songs = new List<Song>()
         {
-            new Song(){Id = 0 , Title = "Willow", Language = "English"},
+            new Song(){Id = 0 , Title = "Willowss", Language = "English"},
             new Song(){Id = 1 , Title = "After Glow", Language = "English"},
         };
 
@@ -30,6 +30,18 @@ namespace MusicApi.Controllers
         public void Post([FromBody]Song song)
         {
             songs.Add(song);
+        }
+
+        [HttpPut("{id}")]
+        public void put(int id, [FromBody] Song song)
+        {
+            songs[id] = song;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            songs.RemoveAt(id);
         }
     }
 }
