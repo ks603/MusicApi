c26e924aabbab7cda516bf284b77867e74b1ad8b
Kevin <kevin.shaughnessy18@gmail.com>
2021-09-21 23:18:42 -0400

    Dropped the database and changed location for new database

--- MusicApi/Startup.cs
+++ MusicApi/Startup.cs
@@ -35,11 +35,11 @@ namespace MusicApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MusicApi", Version = "v1" });
             });
-            services.AddDbContext<ApiDbContext>(option => option.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=MusicDb;"));
+            services.AddDbContext<ApiDbContext>(option => option.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MusicDb;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApiDbContext dbContext)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -48,7 +48,7 @@ namespace MusicApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MusicApi v1"));
             }
 
-            dbContext.Database.EnsureCreated();
+            
 
             app.UseHttpsRedirection();
 
