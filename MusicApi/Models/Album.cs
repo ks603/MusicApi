9753b6272de19efa9cd6d3dd931fc1bdcd79491b
Kevin <kevin.shaughnessy18@gmail.com>
2021-12-14 22:26:52 -0500

    Created Album class

--- MusicApi/Models/Album.cs
+++ MusicApi/Models/Album.cs
@@ -5,12 +5,12 @@ using System.Threading.Tasks;
 
 namespace MusicApi.Models
 {
-    public class Artist
+    public class Album
     {
         public int Id { get; set; }
-        public string MyProperty { get; set; }
-        public string Gender { get; set; }
+        public String Name { get; set; }
         public string ImageUrl { get; set; }
+        public int ArtistId { get; set; }
 
     }
 }
