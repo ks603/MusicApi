83b19d7e459fd845aab3d662f61cbded76bfe3f3
Kevin <kevin.shaughnessy18@gmail.com>
2021-12-16 23:13:39 -0500

    Created ArtistsController

---
+++ MusicApi/Controllers/ArtistsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistsController : ControllerBase
+    {
+        private ApiDbContext _dbContext;
+        public ArtistsController(ApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
