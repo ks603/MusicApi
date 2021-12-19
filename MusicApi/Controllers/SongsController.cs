17517f86eccc602f2d6f63c876d3677784712b92
Kevin <kevin.shaughnessy18@gmail.com>
2021-12-18 19:06:55 -0500

    Created Post for SongsController

---
+++ MusicApi/Controllers/SongsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicApi.Data;
+using MusicApi.Helper;
+using MusicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private ApiDbContext _dbContext;
+
+        public SongsController(ApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] Song song)
+        {
+            var imageUrl = await FileHelper.UploadImage(song.Image);
+            song.ImageUrl = imageUrl;
+            await _dbContext.Songs.AddAsync(song);
+            await _dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
